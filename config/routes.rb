c10ef91261dc041c24b7944a52d009ddc3a81464
Katarzyna Kasprzak <kasia.kasprzak18@gmail.com>
2015-10-13 14:52:21 +0200

    add associations, add some routes and create registrations view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  root to: "pages#about"
+
+  resources :teachers
+
+  resources :reports do
+    get :subjects
+  end
 
   resources :students do
     get :subjects
